146d240feb3d5dcb19bedf2e5cf74bbb9c0c9f6c
AJEET SINGH <ajeet.singh.cs23@iilmcet.ac.in>
2023-06-17 14:02:05 +0530

    Update some code for authentication

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQXG0_V80JqU_qf8wQ6Kyr4DOrBkRhUGE",
+  authDomain: "my-book-store-d1827.firebaseapp.com",
+  projectId: "my-book-store-d1827",
+  storageBucket: "my-book-store-d1827.appspot.com",
+  messagingSenderId: "290020397483",
+  appId: "1:290020397483:web:c2525dc65572b6af4fc2ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
